8c2645fa3cdc2f650d09d3214e756276b2740e97
hnbwww <52497032+hnbwww@users.noreply.github.com>
2023-12-03 22:31:07 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import requests
 
 app = Flask(__name__)
 
@@ -9,3 +10,21 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route('/get_qb')
+def get_qb():
+    qb_id = request.args.get('ID')
+    if qb_id:
+        url = f'https://api.qubic.li/PublicPool/Performance/{qb_id}'
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            qb_data = response.json()
+            return jsonify(qb_data)
+        else:
+            return 'Failed to fetch QB data', response.status_code
+    else:
+        return 'Please provide an ID parameter for /get_qb route'
+
+if __name__ == '__main__':
+    app.run()
