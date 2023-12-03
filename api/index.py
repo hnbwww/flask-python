74111792e47f5c79dc5ffbb1fab3e6a130ecc5e2
hnbwww <52497032+hnbwww@users.noreply.github.com>
2023-12-04 00:17:18 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,12 +1,72 @@
 from flask import Flask, render_template,jsonify, request
 import requests
+import json
+import time
+import os
+
 
 app = Flask(__name__)
+
+
+
+def fetch_data():
+    try:
+        response = requests.get('https://flask-python-lemon.vercel.app/get_qb?ID=QQDGSVIXBLDOWDHSTKKUCTOHDFRAZAXITNUHOJNWSACECKGUSGSTONPDZMDI')
+        data = response.json()
+        return data
+    except Exception as e:
+        print('Error fetching data:', e)
+        return None
+
+def calculate_total_diff(last_data, current_data):
+    if last_data is None or current_data is None:
+        return None, None
+
+    last_solutions_found_total = sum(entry['solutionsFound'] for entry in last_data)
+    last_current_its_total = sum(entry['currentIts'] for entry in last_data)
+    current_solutions_found_total = sum(entry['solutionsFound'] for entry in current_data)
+    current_current_its_total = sum(entry['currentIts'] for entry in current_data)
+
+    solutions_diff = current_solutions_found_total - last_solutions_found_total
+    its_diff = current_current_its_total - last_current_its_total
+    lastActive = last_data[0].lastActive
+
+    return lastActive,solutions_diff, its_diff
+
+def save_current_data(data):
+    with open('current_data.json', 'w') as file:
+        json.dump(data, file)
+
+def load_last_data():
+    if os.path.exists('current_data.json'):
+        with open('current_data.json', 'r') as file:
+            return json.load(file)
+    else:
+        return None
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/diff')
+def diff():
+    last_data = load_last_data()
+    current_data = fetch_data()
+    
+    if current_data:
+        solutions_diff, its_diff = calculate_total_diff(last_data, current_data)
+        if solutions_diff is not None and its_diff is not None:
+            print(f'Total solutionsFound difference since last hour: {solutions_diff}')
+            print(f'Total currentIts difference since last hour: {its_diff}')
+        else:
+            print('Failed to calculate differences.')
+
+        save_current_data(current_data)
+    pass
+    
+    return {'last_time':last_time,'solutions_diff': solutions_diff, 'its_diff': its_diff}
+
+    
 
 @app.route('/about')
 def about():
